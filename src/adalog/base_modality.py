59a6eac743159d5589a8428ef67bf74f300b36dc
PhilippThoelke <philipp.thoelke@posteo.de>
2025-06-28 00:30:00 +0200

    Add back BaseModalityRec

--- src/adalog/base_modality.py
+++ src/adalog/base_modality.py
@@ -4,6 +4,25 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QWidget
 
 
+class BaseModalityRec(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # ensure the QSS background rules are honoured
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+
+    @abstractmethod
+    def setup_ui(self):
+        pass
+
+    def start_recording(self, session_dir):
+        """Called when a session starts. Can be overridden."""
+        pass
+
+    def stop_recording(self):
+        """Called when a session stops. Can be overridden."""
+        pass
+
+
 class BaseModalityOn(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -78,4 +97,5 @@ class BaseModalityOff(QWidget):
     #
     # No abstract methods required for inspection-only panels.
     #
+    # No abstract methods required for inspection-only panels.
     # No abstract methods required for inspection-only panels.
